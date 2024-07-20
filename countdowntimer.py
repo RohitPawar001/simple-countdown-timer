a8846c02ee587cd635152aa7515feb343bde18d3
RohitPawar001 <rppawar491@gmail.com>
2024-07-20 21:47:39 +0530

    updated code

--- countdowntimer.py
+++ countdowntimer.py
@@ -1,13 +1,21 @@
 import time
 
-my_time = int(input("enter the time inseconds"))
 
-for x in range(my_time,0,-1):
-    secs = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x/3600) % 60
+def countdown_timer(remaining_time):
+    for x in range(remaining_time,0,-1):
+        secs = x % 60
+        minutes = int(x / 60) % 60
+        hours = int(x/3600) % 60
     
-    print(f"{hours:02}:{minutes:02}:{secs:02}",end="\r")
-    time.sleep(1)
+        print(f"{hours:02}:{minutes:02}:{secs:02}",end="\r")
+        time.sleep(1)  
+    print("TIME'S UP")
     
-print("TIME'S UP")
+
+def main():
+    my_time = int(input("enter the time inseconds"))
+    countdown_timer(my_time)
+    
+    
+if __name__ == "__main__":
+    main()
